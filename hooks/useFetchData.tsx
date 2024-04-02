32f8be5528b29c45abb7634ee693bcc0063b4a15
Sarthak0085 <sarth.mahajan@gmail.com>
2024-04-02 12:48:05 +0530

    add .env file

--- hooks/useFetchData.tsx
+++ hooks/useFetchData.tsx
@@ -18,9 +18,7 @@ const useFetchData = <T,>(): FetchDataResponse<T> => {
 
     const fetchData = async () => {
       try {
-        const res = await fetch(
-          "https://portfolio-backend-30mp.onrender.com/api/v1/get/user/65b3a22c01d900e96c4219ae"
-        );
+        const res = await fetch(process.env.NEXT_PUBLIC_SERVER_URL as string);
         if (!res.ok) {
           throw new Error("Failed to fetch data");
         }
